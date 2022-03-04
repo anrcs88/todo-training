31118437dbdd579b25f931ed8bf44839e934ae02
anrcs88 <anrcs88@gmail.com>
2022-03-04 19:28:30 +0100

    zbedne

--- src/app/pages/first-page.page.ts
+++
@@ -1,5 +0,0 @@
-import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
-
-@Component({ selector: 'lib-first-page-page', templateUrl: './first-page.page.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
-export class FirstPagePage {
-}
